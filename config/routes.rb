2f3c8dd7cffc38d9948f7b14e77262217a8bb007
Shouya Higuchi <44918658+shouya31@users.noreply.github.com>
2021-04-02 15:00:04 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'posts#index' # 本番環境で開くときに必要なため、仮置きしている
-  get 'posts', to: 'posts#index'  # ※カリキュラムではこちらのみ設定する仕様。上はいらない。
-
-
+  root 'posts#index'
+  get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
-    
 end
