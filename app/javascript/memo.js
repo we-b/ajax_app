50464c89259fa43fb14f7f30575742450e559fef
tiyodakoki <kagunatsu@gmail.com>
2022-08-04 18:05:08 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,15 +1,14 @@
 function post (){
-  //リクエストを送信する処理
-const submit = document.getElementById("submit")
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  const form = document.getElementById("form");
-  const formDate = new FormDate(form);
-  const XHR = new XMLHttpRequest();
-  XHR.open("POST", "/posts", true);
-  XHR.responseType = "json";
-  XHR.send(formData);
-});
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
 };
 
 window.addEventListener('load', post);
