39771b3a6c61912162acce3647cf1e1f6c2a54df
allfiction100806210913 <lili10080621@gmail.com>
2022-09-02 17:01:52 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,12 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new
-  end
+  # コメントアウトする
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
